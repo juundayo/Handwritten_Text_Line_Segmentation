25939ce7c4d5db4dde245f1b97a02881127c6cb7
June <166293934+juundayo@users.noreply.github.com>
2025-09-06 10:48:38 +0300

    Code cleanup.

--- src/line_segmentation/line_segmentation.py
+++ src/line_segmentation/line_segmentation.py
@@ -1,7 +1,7 @@
-# Utils
 import logging
 import os
 import time
+import cv2
 import argparse
 
 import src.line_segmentation.preprocessing.energy_map
@@ -14,82 +14,75 @@ from src.line_segmentation.utils.XMLhandler import writePAGEfile
 from src.line_segmentation.utils.graph_logger import GraphLogger
 from src.line_segmentation.utils.util import create_folder_structure, save_img
 
+# ----------------------------------------------------------------------------#
 
-#######################################################################################################################
+IMG_PATH = '/home/ml3/Desktop/Thesis/.venv/LineSegmentation/text-segmentation/data/Screenshot_22.png'
 
+# ----------------------------------------------------------------------------#
 
 def extract_textline(input_path: str, output_path: str, penalty_reduction: int, seam_every_x_pxl: int,
                      testing: bool, vertical: bool, console_log: bool, small_component_ratio: float):
     """
-    Function to compute the text lines from a segmented image. This is the main routine where the magic happens
+    Function to compute the text lines from a segmented image. 
     """
 
     # -------------------------------
     start_whole = time.time()
     # -------------------------------
 
-    ###############################################################################################
-    # Load the image
+    # Image loading
     img = load_image(input_path)
 
-    ###############################################################################################
-    # Creating the folders and getting the new root folder
+    # Creating the folders and getting the new root folder.
     root_output_path = create_folder_structure(input_path, output_path, (penalty_reduction, seam_every_x_pxl, small_component_ratio))
 
-    # Init the logger with the logging path
+    # Init the logger with the logging path.
     init_logger(root_output_path, console_log)
 
-    # Init the graph logger
+    # Init the graph logger.
     GraphLogger.IMG_SHAPE = img.shape
     GraphLogger.ROOT_OUTPUT_PATH = root_output_path
 
-    ###############################################################################################
-    # Prepare image (filter only text, ...)
+    # Image preparation.
     img = prepare_image(img, testing=testing, cropping=False, vertical=vertical)
-    # Pre-process the image
     save_img(img, path=os.path.join(root_output_path, 'preprocess', 'original.png'))
+
+    # Image pre-processing.
     img = preprocess(img, small_component_ratio)
     save_img(img, path=os.path.join(root_output_path, 'preprocess', 'after_preprocessing.png'))
-
-    ###############################################################################################
-    # Create the energy map
+    
+    # Energy map creation.
     energy_map, connected_components = src.line_segmentation.preprocessing.energy_map.create_energy_map(img,
                                                                                       blurring=False,
                                                                                       projection=False,
                                                                                       asymmetric=True)
-    # Visualize the energy map as heatmap
+    # Visualizing the energy map as heatmap.
     heatmap = src.line_segmentation.preprocessing.energy_map.create_heat_map_visualization(energy_map)
     save_img(heatmap, path=os.path.join(root_output_path, 'energy_map', 'energy_map_without_seams.png'))
 
-    ###############################################################################################
-    # Get the seams
+    # Getting the seams and drawing them on the heatmap.
     seams = get_seams(energy_map, penalty_reduction, seam_every_x_pxl)
-    # Draw the seams on the heatmap
     draw_seams(heatmap, seams)
     save_img(heatmap, path=os.path.join(root_output_path, 'energy_map', 'energy_map_with_seams.png'))
-    # Post-process the seams
+
+    # Seams post-processing and drawing them on the heatmap.
     seams = post_process_seams(energy_map, seams)
-    # Draw the seams on the heatmap
     draw_seams_red(heatmap, seams)
     save_img(heatmap, path=os.path.join(root_output_path, 'energy_map', 'energy_map_postprocessed_seams.png'))
 
-    ###############################################################################################
-    # Extract the bins
+    # Bin extraction.
     lines, centroids, values = majority_voting(connected_components, seams)
 
-    # Draw the bins on a white gray image of the text with red seams
+    # Draw the bins on a white gray image of the text with red seams.
     # draw_bins(img, centroids, root_output_path, seams, values)
-    ###############################################################################################
-    # Get polygons from lines
+
+    # Getting polygons from the lines and drawing them on the original image.
     polygons = get_polygons_from_lines(img, lines, connected_components, vertical)
-    # Draw polygons overlay on original image
     # save_img(draw_polygons(img.copy(), polygons, vertical), path=os.path.join(root_output_path, 'polygons_on_text.png'))
 
-    ###############################################################################################
-    # Write the results on the XML file
+    # Writing the results on the XML file.
     writePAGEfile(os.path.join(root_output_path, 'polygons.xml'), polygon_to_string(polygons))
 
-    ###############################################################################################
     # -------------------------------
     stop_whole = time.time()
     logging.info("finished after: {diff} s".format(diff=stop_whole - start_whole))
@@ -97,33 +90,33 @@ def extract_textline(input_path: str, output_path: str, penalty_reduction: int, 
 
     return
 
+# ----------------------------------------------------------------------------#
 
-#######################################################################################################################
 def init_logger(root_output_path, console_log):
-    # create a logging format
+    # Creating a logging format.
     formatter = logging.Formatter(fmt='%(asctime)s %(filename)s:%(funcName)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    # get the logger
+    # Getting the logger.
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    # create and add file handler
+
+    # Creating and adding a file handler.
     handler = logging.FileHandler(os.path.join(root_output_path, 'logs', 'extract_textline.log'))
     handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     if console_log:
-        # create and add stderr handler
+        # Creating and adding a stderr handler.
         stderr_handler = logging.StreamHandler()
         stderr_handler.formatter = formatter
         logger.addHandler(stderr_handler)
 
-#######################################################################################################################
-
+# ----------------------------------------------------------------------------#
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', type=str, default='./src/data/test1.png', help='Path to the input file')
+    parser.add_argument('--input-path', type=str, default=IMG_PATH, help='Path to the input file')
     parser.add_argument('--output-path', type=str, default='./output', help='Path to the output folder')
     parser.add_argument('--seam-every-x-pxl', type=int, default=100, help='After how many pixel a new seam should be'
                                                                           ' casted')
